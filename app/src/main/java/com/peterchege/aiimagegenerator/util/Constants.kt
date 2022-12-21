8d814f7bd016698766537e140841858e7255ff8a
Peter Chege <peterkagure@gmail.com>
2022-12-22 00:41:05 +0300

    Initial Commit

---
+++ app/src/main/java/com/peterchege/aiimagegenerator/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.peterchege.aiimagegenerator.util
+
+object Constants {
+    const val OPEN_AI_API_KEY = "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"
+
+    const val BASE_URL = "https://api.openai.com/v1/images/generations"
+}
