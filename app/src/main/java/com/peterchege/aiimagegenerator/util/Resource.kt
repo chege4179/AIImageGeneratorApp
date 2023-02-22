124491e6c5e7911875a9c105a130c7535bc808bb
Peter Chege <peterkagure@gmail.com>
2023-02-22 03:06:19 +0300

    Added spotless to new files

--- app/src/main/java/com/peterchege/aiimagegenerator/util/Resource.kt
+++ app/src/main/java/com/peterchege/aiimagegenerator/util/Resource.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 PInstagram
+ * Copyright 2023 AI Image Generator App Peter Chege Mwangi
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
